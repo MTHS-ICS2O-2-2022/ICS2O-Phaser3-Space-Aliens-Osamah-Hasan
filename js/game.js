65e3df477d43632c2ef1b3b36c5b576c2d9bed07
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-04-14 16:55:26 +0000

    addad js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+/* global Phaser */
+
+// Copyright (C) 2023 Osamah Hasan All right reserved.
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+//Created by: Osamah Hasan
+//Created on: Apr 2023
+//This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug : true,
+    },
+  },
+  //set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
