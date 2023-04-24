32f4a5c084b8f82909f9061767de70b8608d307c
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-04-24 13:01:01 -0400

    03 update

--- js/game.js
+++ js/game.js
@@ -6,6 +6,12 @@
 //Created on: Apr 2023
 //This is the Phaser3 game configuration file
 
+//scene import statments
+import splashScene from "./splashScene.js"
+
+// create the new scenes
+const splashScene = new splashScene()
+
 /**
  * Start Phaser Game.
  */
@@ -30,3 +36,10 @@ const config = {
 
 const game = new Phaser.Game(config)
 console.log(game)
+
+// load scenes 
+// Note: remeber any "key" is golbal CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+
+// the  start scene
+game.scene.start("splashScene")
---
+++ js/splashScene.js
@@ -0,0 +1,58 @@
+/* global Phaser */
+
+//copyright (c) 2023 Osamah Hasan All right reserved
+//
+//Created by: Osamah Hasan
+//Created on: Apr 2023
+//This is the Splash Scene
+
+/** 
+ * This class is the Splasch Scene.
+ */
+class SplashScene extends Phaser.Scene {
+    /**
+     * This methed is the construtor.
+     */
+    constructor(){
+        super({key: "splashScene"})
+    }
+
+    /**
+     * Can be defind on your own Scenes.
+     * This methed is called by the Scene Manager when the scene start,
+     *   before pereload() and create().
+     * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+     */
+    Infinity(data) {
+        this.cameras.main.setBackgroundColor("ffffff")
+    }
+
+    /**
+     * Can be defined on your own Scenes.
+     * Use it to load assets.
+     */
+    preload(){
+        console.log("Splash Scene")
+    }
+
+    /**
+     * Can be defined on your own Scenes.
+     * Use it to create your game objects.
+     * @param {object} data - Any data passed via ScenePlugin.add() or ScenePludin.start().
+     */
+    create(data){
+        // pass
+    }
+
+    /**
+     * Should be overriddem by your own Scenes.
+     * This method is called once per game step while scene is running.
+     * @param {number} time - This current time.
+     * @param {number} delta - This delta time in ms since the last frame.
+     */
+    update(time, delta) [
+        // pass 
+    ]
+}
+
+export default SplashScene
